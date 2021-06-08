2e894b866188c9683086a056a4ca85c5c7495ee8
intechcentr <33432711+intechcentr@users.noreply.github.com>
2021-06-08 21:36:21 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,28 @@
+<? php
+
+$ album = 'https://vk.com/photo511254518_456239018' ;
+$ res = parse_url ( $ альбом );
+$ path = substr ( $ res [ 'путь' ], 9 );
+$ arr = explode ( '_' , $ путь );
+$ owner_id = $ arr [ 0 ];
+$ album_id = $ arr [ 1 ];
+
+$ standalone = "by1vPOE2NZ9j6k9MmQsV" ;
+$ group_token = '7080babf86d134287cab24ce19c8961fa38fe4933396f8d68c6aff3db93222de06781332abadcb2373511' ;
+$ conf = [
+	'standalone' => $ standalone ,
+	'group_token' => $ group_token ,
+	'contorm_token' => '34c40d8e' ,
+	'mess' => 'Фото в студио' ,
+	'not_command' => 'Ничего не понял!' ,
+	'owner_id' => $ owner_id ,
+	'album_id' => $ album_id ,
+	'group_id' => '7874730' ,
+	'apiurl' => 'https://api.vk.com/method/' ,
+	'путь' => substr ( $ _SERVER [ 'PHP_SELF' ], 0 , - 2 ),
+	'photos' => 'photos.txt' ,
+	'temp_link' => 'temp_album.txt' ,
+	'random_id' => mt_rand ( 0000000000 , 999999999999 ),
+	'v' => '5.50'
+];
+?>
